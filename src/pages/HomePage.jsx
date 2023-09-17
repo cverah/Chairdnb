f99c4630fc736753cb7288a296225826a409e7ba
Cristhian Vera <cristianmario092190@gmail.com>
2023-09-17 11:54:43 -0500

    feat: add FeatureChair.jsx in HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import Acomodation from "../components/Acomodation/Acomodation";
 import AdventuresChair from "../components/AdventuresChair/AdventuresChair";
 import ChairPlus from "../components/ChairPlus/ChairPlus";
 import ExplorerChair from "../components/ExplorerChair/ExplorerChair";
+import FeaturedChair from "../components/FeaturedChair/FeaturedChair";
 import Header from "../components/Header/Header";
 import HighlyExperiences from "../components/HighlyExperiences/HighlyExperiences";
 import SearchForm from "../components/SearchForm/SearchForm";
@@ -10,7 +11,7 @@ const HomePage = () => {
   const linkAdventures = "/adventures";
   const linkAcommodations = "/accommodations";
   const linkExperiences = "/experiences";
-  // const linkFeatured = "/featured";
+  const linkFeatured = "/featured";
   return (
     <>
       <Header>
@@ -21,6 +22,7 @@ const HomePage = () => {
       <AdventuresChair link={linkAdventures} />
       <Acomodation link={linkAcommodations} />
       <HighlyExperiences link={linkExperiences} />
+      <FeaturedChair link={linkFeatured} />
     </>
   );
 };
