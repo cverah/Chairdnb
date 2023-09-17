638497cfd66e8f4e23697d8937364cb5b4ba3131
Cristhian Vera <cristianmario092190@gmail.com>
2023-09-17 11:44:16 -0500

    feat: add ChairPlus.jsx in HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import ChairPlus from "../components/ChairPlus/ChairPlus";
 import ExplorerChair from "../components/ExplorerChair/ExplorerChair";
 import Header from "../components/Header/Header";
 import SearchForm from "../components/SearchForm/SearchForm";
@@ -9,6 +10,7 @@ const HomePage = () => {
         <SearchForm />
       </Header>
       <ExplorerChair />
+      <ChairPlus />
     </>
   );
 };
