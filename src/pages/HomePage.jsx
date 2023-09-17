59c4e046ea9872550f3885f624501390ac89a0d8
Cristhian Vera <cristianmario092190@gmail.com>
2023-09-17 11:47:20 -0500

    fead: add AdventureChair in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
+import AdventuresChair from "../components/AdventuresChair/AdventuresChair";
 import ChairPlus from "../components/ChairPlus/ChairPlus";
 import ExplorerChair from "../components/ExplorerChair/ExplorerChair";
 import Header from "../components/Header/Header";
 import SearchForm from "../components/SearchForm/SearchForm";
 
 const HomePage = () => {
+  const linkAdventures = "/adventures";
+  // const linkAcommodations = "/accommodations";
+  // const linkExperiences = "/experiences";
+  // const linkFeatured = "/featured";
   return (
     <>
       <Header>
@@ -11,6 +16,7 @@ const HomePage = () => {
       </Header>
       <ExplorerChair />
       <ChairPlus />
+      <AdventuresChair link={linkAdventures} />
     </>
   );
 };
